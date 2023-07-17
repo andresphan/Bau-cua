9f338a335a2ed0ff3766ea5371d5d991e697c969
Phan Nguyen Xuan Loc <99610485+andresphan@users.noreply.github.com>
2023-07-17 10:46:58 +0700

    Delete src/JSS_StyledComponent directory

--- src/JSS_StyledComponent/Themes/ToDoListDarkTheme.js
+++
@@ -1,9 +0,0 @@
-export const ToDoListDarkTheme = {
-    bgColor: '#343a40',
-    color:'#fff',
-    borderButton: '1px solid #fff',
-    borderRadiusButton: 'none',
-    hoverTextColor: '#343a40',
-    hoverBgColor:'#fff',
-    borderColor:'#343a40'
-}
