bed2b94fd3bc03efe5f502390537ffb7b9f2af7a
Phan Nguyen Xuan Loc <99610485+andresphan@users.noreply.github.com>
2023-07-17 10:47:18 +0700

    Delete src/redux directory

--- src/redux/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import ToDoListReducer from './ToDoListReducer'
-import FakeBookReducer from './FakeBookReducer'
-import BaiTapGameBauCuaReducer from './BaiTapGameBauCuaReducer'
-export const rootReducer = combineReducers({
-    ToDoListReducer,
-    FakeBookReducer,
-    BaiTapGameBauCuaReducer
-})
--- src/redux/types/FakeBookType.js
+++
@@ -1,1 +0,0 @@
-export const ADD_COMMENT = "add_comment"; 
